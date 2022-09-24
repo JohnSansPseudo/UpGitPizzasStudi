327e0566d5c42a58af8a6e7f239bc3e87f10399b
JO <jonathan.teyssier@gmail.com>
2022-09-24 13:39:56 +0200

    ajout de la pizza new pizza

--- sources/pizza.py
+++ sources/pizza.py
@@ -35,5 +35,9 @@ RECIPES = {
     'mediterranea': {
         'ingredients': {'mozzarella': 3, 'pepper': 4, 'onion': 3},
         'price': 11,
+    },
+        'new': {
+        'ingredients': {'gorgonzola': 3, 'gorgonzola': 4, 'onion': 3},
+        'price': 15,
     }
 }
